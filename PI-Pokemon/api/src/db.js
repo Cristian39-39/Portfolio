72991adf60b64f5efe1476a0dfdce15fc0517f2a
Cristian Velez <cristian.velez39@outlokk.com>
2021-08-04 21:41:23 -0500

    type_img

--- PI-Pokemon/api/src/db.js
+++ PI-Pokemon/api/src/db.js
@@ -5,11 +5,13 @@ const path = require('path');
 const {
   DB_USER, DB_PASSWORD, DB_HOST,
 } = process.env;
-
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pokemon`, {
+
+//const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pokemon`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
+sequelize.authenticate().then(()=>console.log('success')).catch(e => console.log(e))
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
