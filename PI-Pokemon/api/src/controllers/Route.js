d898034661c1cfa03e5d5e9d8079572ab4b78bba
Cristian Velez <cristian.velez39@outlokk.com>
2021-08-07 13:19:06 -0500

    backend_v1

---
+++ PI-Pokemon/api/src/controllers/Route.js
@@ -0,0 +1,30 @@
+function normalizar (poke){
+    let normalizadoPoke = {
+        id: poke.id,
+        name: poke.name,
+        img: poke.img,
+        types: poke.types.map((t)=>({timg: t.imagen, typename: t.name}))
+    }
+    return normalizadoPoke
+}
+
+function normalizarPokemonDetails (poke){
+    let normalizadoPoke = {
+        id: poke.id,
+        name: poke.name,
+        imagen: poke.img,
+        vida: poke.vida,
+        fuerza: poke.fuerza,
+        defensa: poke.defensa,
+        velocidad: poke.velocidad,
+        altura: poke.altura,
+        peso: poke.peso,
+        types: poke.types.map((t)=>({timg: t.imagen, typename: t.name}))
+    }
+    return normalizadoPoke
+}
+
+module.exports = {
+    normalizar,
+    normalizarPokemonDetails
+}
--- PI-Pokemon/api/src/routes/pokemon.js
+++ PI-Pokemon/api/src/routes/pokemon.js
@@ -1,62 +1,61 @@
 const express = require('express')
 const { Pokemon, Type } = require('../db.js')
-const router = express.Router()
+const {normalizar, normalizarPokemonDetails} = require('../controllers/Route.js')
 const axios = require('axios')
+const router = express.Router()
 
 
-//Buscar todos los pokemon
+//Buscar pokemon one and all
 router.get('/', (req,res,next)=>{
     let pname = req.body.name
-    console.log(pname)
     if(!!pname){
-        Pokemon.findOne( { where: {name: pname} } ).then((onepokemon)=>{
-            console.log(onepokemon)
-            res.send(onepokemon)
-        })
-
-
-
-        // axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`)
-        // .then((apipoke)=>{
-        //     let newPokemon = apipoke.data;
-            
-        //     resPokemon = {
-        //         id: newPokemon.id, 
-        //         name: newPokemon.name, 
-        //         vida: newPokemon.stats[0].base_stat,
-        //         fuerza: newPokemon.stats[1].base_stat,
-        //         defensa: newPokemon.stats[2].base_stat,
-        //         velocidad: newPokemon.stats[5].base_stat,
-        //         altura: newPokemon.height,
-        //         peso: newPokemon.weight,
-        //         types: newPokemon.types.map((t)=>(t.type.name))
-        //     }
-        //     Pokemon.create(resPokemon).then(()=>{
-        //         Pokemon.findByPk(resPokemon.id).then((poke)=>poke.addTypes(resPokemon.types))
-        //     })
-        //     res.send(resPokemon)
-        // })
-
+        Pokemon.findOne( { where: {name: pname}, include: Type } ).then((onepokemon)=>{
+            if(onepokemon){
+                onepokemon = normalizar(onepokemon)
+                res.send(onepokemon)
+            }else{
+                axios.get(`https://pokeapi.co/api/v2/pokemon/${pname}`).then((apipoke)=>{
+                    let newPokemon = apipoke.data;
+                    newPokemon = {
+                        id: newPokemon.id, 
+                        name: newPokemon.name, 
+                        vida: newPokemon.stats[0].base_stat,
+                        fuerza: newPokemon.stats[1].base_stat,
+                        defensa: newPokemon.stats[2].base_stat,
+                        velocidad: newPokemon.stats[5].base_stat,
+                        altura: newPokemon.height,
+                        peso: newPokemon.weight,
+                        types: newPokemon.types.map((t)=>(t.type.name))
+                    }
+                    Pokemon.create(newPokemon).then(()=>{
+                        Pokemon.findByPk(newPokemon.id).then((poke)=>poke.addTypes(newPokemon.types))
+                    }).then(()=>{
+                        Pokemon.findOne( { where: {name: pname}, include: Type } ).then((onepokemon)=>{
+                            onepokemon = normalizar(onepokemon)
+                            res.send(onepokemon)
+                    })
+                })
+                })};
+        }).catch(error=>next(error));     
     }else{
         Pokemon.findAll( {include: Type} ).then(results=>{
             let dbPokemon = results;
         //normalizado
         dbPokemon = dbPokemon.map((p)=>(
-            {
-                id: p.id,
-                name: p.name,
-                img: p.img,
-                types: p.types.map((t)=>({timg: t.imagen, typename: t.name}))
-            }
+            normalizar(p)
         ))
             res.send(dbPokemon)
-        })
-        .catch(error=>next(error))
+        }).catch(error=>next(error))
     }
 })
 
-//Buscar one pokemon
-
+//Pokemon details
+router.get('/:name', (req,res,next)=>{
+    Pokemon.findOne( { where: {name: req.params.name}, include: Type } ).then((onepokemon)=>{
+        onepokemon = normalizarPokemonDetails(onepokemon)
+        res.send(onepokemon)
+})
+})
 
 
 
