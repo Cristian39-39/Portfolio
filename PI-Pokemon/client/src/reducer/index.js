5b21796f17f942b5a2de177e6f70c447bfb28032
Cristian Velez <cristian.velez39@outlokk.com>
2021-08-16 00:56:39 -0500

    finish filter

--- PI-Pokemon/client/src/reducer/index.js
+++ PI-Pokemon/client/src/reducer/index.js
@@ -1,13 +1,18 @@
-import { GET_POKEMON, GET_ALL_POKEMONS, FIND_POKEMON, SELECT_PAGE, RENDER_POKEMON, } from "../actions/consts";
+import { GET_POKEMON, GET_ALL_POKEMONS, FIND_POKEMON,
+     SELECT_PAGE, RENDER_POKEMON, CHANGE_FILTER, FILTER,
+    FILTER_TYPE, FILTER_API, FILTER_DB } from "../actions/consts";
 
 
 var initialState = {
+    pokemonsAll: [],
     pokemons:[],
     pokemonRender:[],
     pokemonDetail:{},
     page: 1,
     filter: 'ALL',
-    order: 'ASC'
+    type: 'normal',
+    order: 'asc',
+    select: 'type'
 }
 
 function reducer(state=initialState, action){
@@ -15,19 +20,19 @@ function reducer(state=initialState, action){
         case GET_ALL_POKEMONS:
             return{
                 ...state,
-                pokemons: action.payload,
+                pokemonsAll: action.payload,
                 page: 1
             }
         case GET_POKEMON:
             return{
                 ...state,
-                pokemons: action.payload.concat(state.pokemons),
+                pokemonsAll: action.payload.concat(state.pokemons),
                 page: 1
             }
         case FIND_POKEMON:
             return{
                 ...state,
-                pokemons: state.pokemons.filter(poke=>poke.name===action.payload).concat(state.pokemons.filter(poke=>poke.name!==action.payload)),
+                pokemonsAll: state.pokemons.filter(poke=>poke.name===action.payload).concat(state.pokemons.filter(poke=>poke.name!==action.payload)),
 
             }
         case SELECT_PAGE:
@@ -40,6 +45,49 @@ function reducer(state=initialState, action){
             return{
                 ...state,
                 pokemonRender: state.pokemons.slice((state.page-1)*9, state.page*9)
+            }
+        case CHANGE_FILTER:
+            return {
+                ...state,
+                filter: action.payload[0],
+                type: action.payload[1]
+            }
+        case FILTER:
+            return {
+                ...state,
+                pokemons: state.pokemonsAll,
+                filterS: false
+            }
+        case FILTER_TYPE:
+            return {
+                ...state,
+                pokemons: state.pokemonsAll.filter((poke)=>
+                poke.types.length
+                    ? poke.types[0].typename === state.type
+                        ? true
+                        : poke.types.length > 1
+                            ? poke.types[1].typename === state.type
+                                ? true
+                                : false
+                            : false
+                    : false),
+                page: 1
+            }
+        case FILTER_API:
+            return {
+                ...state,
+                pokemons: state.pokemonsAll.filter((poke)=>
+                poke.id?poke.id.includes('myPoke')?false:true:false
+                ),
+                page: 1
+            }
+        case FILTER_DB:
+            return {
+                ...state,
+                pokemons: state.pokemonsAll.filter((poke)=>
+                poke.id?poke.id.includes('myPoke')?true:false:false
+                ),
+                page: 1
             }
 
         default: return state
